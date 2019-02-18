f7861f925f8bd6c7b4e8e47754e250f2a78b86a3
Dave Herzog <davidjherzog@gmail.com>
2019-02-18 13:20:10 -0500

    Scope CSS and single quotes

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -34,8 +34,8 @@
       <div class="column is-three-quarters">
        <h1 class="title">{{ selectedMovie.title }}</h1>
        <h4 class="subtitle">
-        <p class="subtitle-tag">{{ selectedMovie.duration }}</p>
-        <p class="subtitle-tag">{{ selectedMovie.genre }}</p>
+        <p class="subtitle-tag">{{ selectedMovie.duration }} |</p>
+        <p class="subtitle-tag">{{ selectedMovie.genre }} |</p>
         <p class="subtitle-tag">{{ selectedMovie.releaseDate }}</p>
        </h4>
        <p class="description">{{ selectedMovie.description }}</p>
@@ -58,7 +58,7 @@
           Remove from
         </span>
           &nbsp;favorites
-          <i class="fa fa-plus-square"></i>
+          <i class="fa fa-plus-square fa-plus-square-o"></i>
         </a>
        </div>
       </div>
@@ -67,40 +67,172 @@
    </div>
 </template>
 <script>
-  import { mapMutations } from 'vuex';
-  
-  export default {
-    data() {
-      return {
-        selectedMovie: this.$store.getters.movie(this.$route.params.id)
-      };
+import { mapMutations } from 'vuex';
+
+export default {
+  data() {
+    return {
+      selectedMovie: this.$store.getters.movie(this.$route.params.id)
+    };
+  },
+  watch: {
+    $route() {
+      this.selectMovie();
+    }
+  },
+  methods: {
+    selectMovie() {
+      this.selectedMovie = this.$store.getters.movie(this.$route.params.id);
     },
-    watch: {
-      $route() {
-        this.selectMovie();
+    ...mapMutations({
+      addToFavorites(commit) {
+        commit('addToFavorites', {
+          id: this.$route.params.id
+        });
       }
-    },
-    methods: {
-      selectMovie() {
-        this.selectedMovie = this.$store.getters.movie(this.$route.params.id);
-      },
-      ...mapMutations({
-        addToFavorites (commit) {
-          commit('addToFavorites', {
-            id: this.$route.params.id
-          });
-        }
-      })
-      /* addToFavorites() {
+    })
+    /* addToFavorites() {
         this.$store.commit('addToFavorites', {
           id: this.$route.params.id
         })
       } */
-    }
-  };
+  }
+};
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+$small: 589px;
+$medium: 767px;
+$large: 1023px;
+
+.hero-body {
+
+  .container {
+    width: 100%;
+  }
+
+  .nav > .container {
+    -webkit-box-align: stretch;
+    -ms-flex-align: stretch;
+    align-items: stretch;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    min-height: 3.25rem;
+    width: 100%;
+  }
+  .nav {
+    position: absolute;
+    top: 10px;
+    width: 100%;
+    box-shadow: 0px 0px 0px !important;
+
+    .nav-item {
+      .fa-bars {
+        color: #ce2424;
+        font-size: 40px;
+      }
+
+      .tag {
+        font-size: 15px;
+      }
+
+      .title {
+        color: red;
+        font-size: 18px;
+        font-weight: 600;
+        text-align: center;
+        text-transform: uppercase;
+      }
+    }
+
+    .nav-right {
+      .nav-item {
+        float: right;
+      }
+    }
+  }
+
+  .column {
+    padding-left: 50px;
+    min-height: 264px;
+
+    .title {
+      font-size: 45px;
+      font-weight: 600;
+
+      @media screen and (max-width: $medium) {
+        font-size: 35px;
+      }
+    }
+
+    .subtitle {
+      font-size: 15px;
+      font-weight: 600;
+      color: red;
+
+      @media screen and (max-width: $medium) {
+        font-size: 14px;
+      }
+
+      .subtitle-tag {
+        padding-right: 3px;
+        display: inline;
+      }
+    }
+
+    .description {
+      font-size: 15px;
+      margin-bottom: 24px;
+      max-width: 450px;
+    }
+
+    .links {
+      .play-button {
+        background: #ce2424;
+        color: #fff;
+        border-radius: 15px;
+        border: 0px;
+        margin-right: 20px;
+        padding: 15px;
+
+        .fa-play {
+          padding-left: 7px;
+        }
+
+        @media screen and (max-width: $medium) {
+          font-size: 14px;
+        }
+      }
+
+      .favorites-button {
+        background: none;
+        text-decoration: none;
+        color: #fff;
+
+        .fa-plus-square-o {
+          padding-left: 10px;
+          padding-top: 2px;
+        }
+
+        @media screen and (max-width: $medium) {
+          background: #fff;
+          color: #4a4a4a;
+          border-radius: 15px;
+          font-size: 14px;
+
+          &:hover {
+            background: #fff;
+          }
+        }
+      }
+
+      @media screen and (max-width: $medium) {
+        text-align: center;
+      }
+    }
+  }
+}
 
 .favorite-shadow {
   box-shadow: 0 0 50px 15px rgba(251, 255, 15, 0.25);
@@ -109,26 +241,4 @@
 .hide {
   display: none;
 }
-
-//@import '~bulma';
-
-/* $small: 589px;
-$medium: 767px;
-$large: 1023px;
-
-.favorite-shadow {
-  box-shadow: 0 0 50px 15px rgba(251, 255, 15, 0.25);
-}
-.favorite-check {
-  position: absolute;
-  right: 5px;
-  top: 5px;
-  z-index: 1;
-  color: #fcff4c;
-
-  @media(max-width: $medium) {
-   position: initial;
-   display: block;
-  }
-} */
 </style>
